f68a5e36d82c76ef71de9826f689df7aa9da47d4
Fi0x <felpech@aol.com>
2021-11-09 19:00:02 +0100

    Pause menu is active at start

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,15 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    [HideInInspector] public static bool IsPaused;
+    [SerializeField] public static bool IsPaused = true;
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private KeyCode pauseKey = KeyCode.Escape;
+
+    private void Start()
+    {
+        if (IsPaused)
+        {
+            pauseMenu.SetActive(IsPaused);
+            Time.timeScale = IsPaused ? 0f : 1;
+        }
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(pauseKey))
         {
             IsPaused = !IsPaused;
             pauseMenu.SetActive(IsPaused);
