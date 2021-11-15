6eaf5915fa3cd785ca0b69a142b5ff365d35db1f
holyhibiskus <tomarf@von-grundies.de>
2021-11-15 20:21:56 +0100

    small fixes

--- Assets/Scripts/Components/SpaceDustVFXG.cs
+++ Assets/Scripts/Components/SpaceDustVFXG.cs
@@ -9,11 +9,6 @@ public class SpaceDustVFXG : MonoBehaviour
     [SerializeField] private VisualEffect vfx;
     [SerializeField] private ShipMovementHandler smh;
 
-    private const int ParticlesOverTime = 100;
-    private const float MinParticleSize = 0.1f;
-    private const float MaxParticleSize = 0.2f;
-    private const float MaxLifetime = 1;
-
     void FixedUpdate()
     {
         var maxShipSpeed = this.smh.maxSpeed + this.smh.maxSpeedBoost;
