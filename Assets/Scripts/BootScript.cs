4228f0b9e592f93693105316abd84d63580d015e
Fi0x <felpech@aol.com>
2022-01-20 21:02:47 +0100

    Added Boot script and updated Startup scene

---
+++ Assets/Scripts/BootScript.cs
@@ -0,0 +1,41 @@
+using Manager;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BootScript : MonoBehaviour
+{
+    [SerializeField] private string levelSceneName;
+    [SerializeField] private GameObject overlayMenuPrefab;
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private GameObject gameControllerPrefab;
+
+    private bool loadStarted;
+    
+    private void Start()
+    {
+        if(this.loadStarted)
+            return;
+        
+        this.loadStarted = true;
+        
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.LoadScene(this.levelSceneName);
+        
+        SceneManager.sceneLoaded += (arg0, mode) =>
+        {
+            if (arg0.name.Equals(this.levelSceneName))
+                this.LoadFirstScene();
+        };
+    }
+
+    private void LoadFirstScene()
+    {
+        Instantiate(this.overlayMenuPrefab);
+        var playerObject = Instantiate(this.playerPrefab);
+        var gameManagerScript = Instantiate(this.gameControllerPrefab).GetComponent<GameManager>();
+        
+        gameManagerScript.NotifyAboutNewPlayerInstance(playerObject);
+
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -3,7 +3,6 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[InitializeOnLoad]
 public class Startup
 {
     static Startup()
