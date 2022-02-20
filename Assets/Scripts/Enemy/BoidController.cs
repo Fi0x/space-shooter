cf71d4d788a3ee0386e5d8a5fc460025eb48ace2
Dominik <domgalp@gmail.com>
2022-02-20 15:12:55 +0100

    Fix boids
    Boid speed NOT dependant on Ship TotalMaxSpeed

--- Assets/Scripts/Enemy/BoidController.cs
+++ Assets/Scripts/Enemy/BoidController.cs
@@ -1,122 +1,166 @@
 using System.Collections;
 using System.Collections.Generic;
-using Enemy;
 using UnityEngine;
 using Manager;
+using Enemy;
 
-namespace Enemy
+public class BoidController : MonoBehaviour
 {
-    public class BoidController : MonoBehaviour
+    [Header("Spawn Setup")]
+    [SerializeField] private Boid flockUnitPrefab;
+    [SerializeField] private int flockSize;
+    [SerializeField] private Vector3 spawnBounds;
+
+    [Header("Speed Setup")]
+    [Range(0, 100)]
+    [SerializeField] private float _minSpeed;
+    public float minSpeed { get { return _minSpeed; } }
+    [Range(0, 100)]
+    [SerializeField] private float _maxSpeed;
+    public float maxSpeed { get { return _maxSpeed; } }
+
+    [Header("Detection Distances")]
+    [Range(0, 100)]
+    [SerializeField] private float _cohesionDistance;
+    public float cohesionDistance { get { return _cohesionDistance; } }
+    [Range(0, 100)]
+    [SerializeField] private float _avoidanceDistance;
+    public float avoidanceDistance { get { return _avoidanceDistance; } }
+    [Range(0, 100)]
+    [SerializeField] private float _alignmentDistance;
+    public float alignmentDistance { get { return _alignmentDistance; } }
+    [Range(0, 20)]
+    [SerializeField] private float _obstacleDistance;
+    public float obstacleDistance { get { return _obstacleDistance; } }
+
+    [Header("Behavior Weights")]
+    [Range(0, 10)]
+    [SerializeField] private float _cohesionWeight;
+    public float cohesionWeight { get { return _cohesionWeight; } }
+    [Range(0, 10)]
+    [SerializeField] private float _avoidanceWeight;
+    public float avoidanceWeight { get { return _avoidanceWeight; } }
+    [Range(0, 10)]
+    [SerializeField] private float _alignmentWeight;
+    public float alignmentWeight { get { return _alignmentWeight; } }
+    [Range(0, 100)]
+    [SerializeField] private float _obstacleWeight;
+    public float obstacleWeight { get { return _obstacleWeight; } }
+
+
+    [Header("Roaming Position")]
+    [SerializeField] private Vector3 _roamingPosition;
+    public Vector3 roamingPosition { get { return _roamingPosition; } }
+    [SerializeField] private float roamingPosReachedDistance;
+    [SerializeField] private float minDistanceFromPlayer;
+    [SerializeField] private float maxDistanceFromPlayer;
+
+    //public Boid[] allUnits { get; set; }
+    public List<Boid> allUnits { get; set; }
+
+    private void Start()
     {
-        [Header("Spawn Setup")]
-        [SerializeField] private Boid flockUnitPrefab;
-        [SerializeField] private int flockSize;
+        GenerateUnits();
+        GetRoamingPosition();
+    }
 
-        [Header("Speed Setup")]
-        [Range(0, 100)]
-        [SerializeField] private float minSpeed;
-        public float MinSpeed => this.minSpeed;
-
-        [Range(0, 100)]
-        [SerializeField] private float maxSpeed;
-        public float MaxSpeed => this.maxSpeed;
-
-        [Header("Detection Distances")]
-        [Range(0, 100)]
-        [SerializeField] private float cohesionDistance;
-        public float CohesionDistance => this.cohesionDistance;
-
-        [Range(0, 100)]
-        [SerializeField] private float avoidanceDistance;
-        public float AvoidanceDistance => this.avoidanceDistance;
-
-        [Range(0, 100)]
-        [SerializeField] private float alignmentDistance;
-        public float AlignmentDistance => this.alignmentDistance;
-
-        [Range(0, 20)]
-        [SerializeField] private float obstacleDistance;
-        public float ObstacleDistance => this.obstacleDistance;
-
-        [Header("Behavior Weights")]
-        [Range(0, 10)]
-        [SerializeField] private float cohesionWeight;
-        public float CohesionWeight => this.cohesionWeight;
-
-        [Range(0, 10)]
-        [SerializeField] private float avoidanceWeight;
-        public float AvoidanceWeight => this.avoidanceWeight;
-
-        [Range(0, 10)]
-        [SerializeField] private float alignmentWeight;
-        public float AlignmentWeight => this.alignmentWeight;
-
-        [Range(0, 100)]
-        [SerializeField] private float obstacleWeight;
-        public float ObstacleWeight => this.obstacleWeight;
-
-
-        [Header("Roaming Position")]
-        [SerializeField] private Vector3 roamingPosition;
-        public Vector3 RoamingPosition => this.roamingPosition;
-        [SerializeField] private float roamingPosReachedDistance;
-        [SerializeField] private float minDistanceFromPlayer;
-        [SerializeField] private float maxDistanceFromPlayer;
-
-        public List<Boid> AllUnits { get; private set; }
-
-        private void Start()
+    /*
+    private void Update()
+    {
+        for (int i = 0; i < allUnits.Length; i++)
         {
-            this.GenerateUnits();
-            this.GetRoamingPosition();
-        }
-
-        private void GenerateUnits()
-        {
-            this.AllUnits = new List<Boid>();
-
-            for(var i = 0; i < this.flockSize; i++)
+            if (Vector3.Distance(allUnits[i].myTransform.position, roamingPosition) <= roamingPosReachedDistance)
             {
-                var randomVector = this.transform.position + Random.onUnitSphere * 10;
-                var rotation = Quaternion.Euler(0, 0, 0);
-                var boid = Instantiate(this.flockUnitPrefab, randomVector, rotation);
-                boid.AssignFlock(this);
-                boid.InitializeSpeed(Random.Range(this.MinSpeed, this.MaxSpeed));
-                boid.BoidHelper();
-                boid.GetComponent<EnemyAI>().InitializeEnemyAI(this);
-
-                // parent
-                boid.transform.SetParent(this.transform);
-                // Add to List
-                this.AllUnits.Add(boid);
-
-                // notify about sensor target
-                GameManager.Instance.EnemyManager.NotifyAboutNewEnemySpawned(boid.gameObject);
+                GetRoamingPosition();
             }
-        }
 
-        private void GetRoamingPosition()
+            //allUnits[i].MoveUnit();
+        }
+    }
+    */
+
+    /*
+    private void GenerateUnits()
+    {
+        allUnits = new Boid[flockSize];
+
+        for (int i = 0; i < flockSize; i++)
         {
-            Vector3 randomDirection = new Vector3(
-                Random.Range(-1f, 1f),
-                Random.Range(-1f, 1f),
-                Random.Range(-1f, 1f));
-
-            Vector3 newRoamingPosition = GameManager.Instance.Player.transform.position + randomDirection 
-                * Random.Range(this.minDistanceFromPlayer, this.maxDistanceFromPlayer);
-
-            this.roamingPosition = newRoamingPosition;
+            var randomVector = Random.insideUnitSphere;
+            randomVector = new Vector3(randomVector.x * spawnBounds.x, randomVector.y * spawnBounds.y, randomVector.z * spawnBounds.z);
+            var spawnPosition = transform.position + randomVector;
+            var rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0, 360), 0);
+            allUnits[i] = Instantiate(flockUnitPrefab, spawnPosition, rotation);
+            allUnits[i].AssignFlock(this);
+            allUnits[i].InitializeSpeed(UnityEngine.Random.Range(minSpeed, maxSpeed));
+            allUnits[i].BoidHelper();
         }
+    }*/
 
-        public void SetNewRoamingPosition()
+    private void GenerateUnits()
+    {
+        allUnits = new List<Boid>();
+
+        for (int i = 0; i < flockSize; i++)
         {
-            this.GetRoamingPosition();
+            /*
+            var randomVector = transform.position + Random.onUnitSphere * 10;
+            var rotation = Quaternion.Euler(0, 0, 0);
+            allUnits[i] = Instantiate(flockUnitPrefab, randomVector, rotation);
+            allUnits[i].AssignFlock(this);
+            allUnits[i].InitializeSpeed(UnityEngine.Random.Range(minSpeed, maxSpeed));
+            allUnits[i].BoidHelper();
+            allUnits[i].GetComponent<EnemyAI>().InitiliazeEnemyAI(this);
+
+            // parent
+            allUnits[i].transform.SetParent(this.transform);
+            */
+
+            var randomVector = transform.position + Random.onUnitSphere * 10;
+            var rotation = Quaternion.Euler(0, 0, 0);
+            Boid boid = Instantiate(flockUnitPrefab, randomVector, rotation);
+            boid.AssignFlock(this);
+            boid.InitializeSpeed(UnityEngine.Random.Range(minSpeed, maxSpeed));
+            boid.BoidHelper();
+            boid.GetComponent<EnemyAI>().InitializeEnemyAI(this);
+
+            // parent
+            boid.transform.SetParent(this.transform);
+            // Add to List
+            allUnits.Add(boid);
+
+            // notify about sensortarget
+            Manager.GameManager.Instance.EnemyManager.NotifyAboutNewEnemySpawned(boid.gameObject);
         }
-    
-        // removes Boid from allUnits
-        public void RemoveBoid(Boid boid)
-        {
-            this.AllUnits.Remove(boid);
-        }
+    }
+
+    private void GetRoamingPosition()
+    {
+        Vector3 randomDirection = new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f),
+            UnityEngine.Random.Range(-1f, 1f));
+
+        Vector3 newRoamingPosition = GameManager.Instance.Player.transform.position + randomDirection
+            * UnityEngine.Random.Range(minDistanceFromPlayer, maxDistanceFromPlayer);
+
+        _roamingPosition = newRoamingPosition;
+
+        /*
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = newRoamingPosition;
+        cube.GetComponent<MeshRenderer>().material.color = Color.red;
+        */
+    }
+
+    public void SetNewRoamingPosition()
+    {
+        GetRoamingPosition();
+    }
+
+
+
+    // removes Boid from allUnits
+    public void RemoveBoid(Boid boid)
+    {
+        allUnits.Remove(boid);
     }
 }
