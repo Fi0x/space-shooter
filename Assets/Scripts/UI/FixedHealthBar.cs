24656dbff43b8977a3709ffcff0e981ba9f6dd65
holyhibiskus <tomarf@von-grundies.de>
2022-02-28 18:03:31 +0100

    Fixed final HealthBar bugs and added option to not generate bar on canvas

--- Assets/Scripts/UI/FixedHealthBar.cs
+++ Assets/Scripts/UI/FixedHealthBar.cs
@@ -1,12 +1,37 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class FixedHealthBar : MonoBehaviour
     {
-        public void SetCurrentHealth(float currentHealth)
+        [Header("Healthbar")]
+        //
+        [SerializeField]
+        private Slider slider;
+        [SerializeField]
+        private TextMeshProUGUI healthNumber_TMP;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            
+
+        }
+
+        public void SetMaxHealth(int maxHealth)
+        {
+            this.slider.maxValue = maxHealth;
+            this.slider.value = maxHealth;
+
+            this.healthNumber_TMP.text = maxHealth + " / " + maxHealth;
+        }
+
+        public void SetCurrentHealth(int health)
+        {
+            this.slider.value = health;
+
+            this.healthNumber_TMP.text = this.slider.value + " / " + this.slider.maxValue;
         }
     }
 }
