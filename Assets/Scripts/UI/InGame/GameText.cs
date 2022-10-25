fea939ad3b4fc1f525edc2408831e157417bca52
Fi0x <58072122+Fi0x@users.noreply.github.com>
2022-10-25 19:27:28 +0200

    2 add a tutorial (#51)
    * Adjusted location and size of tutorial texts
    
    * Adjusted font and position of text-overlay
    
    * Added ids to in-game texts for later reference in the bars
    
    * Replaced repeating texts with updating texts
    
    * Added in-game-texts for more events and levels

---
+++ Assets/Scripts/UI/InGame/GameText.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI.InGame
+{
+    public class GameText : MonoBehaviour
+    {
+        public string CustomId { get; set; }
+        public bool PermanentText { get; set; }
+    }
+}
